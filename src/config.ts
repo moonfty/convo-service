16ddebf0fe3dd9e05c7c65044ac9996b6e166750
ertugdlk <ertgdlk@gmail.com>
2022-02-04 17:13:19 +0300

    mongodb connection test

--- src/config.ts
+++ src/config.ts
@@ -29,4 +29,5 @@ export const mongoPort = process.env.MONGO_DB_PORT || 27017;
 export const mongoDbConnectionString =
     process.env.NODE_ENV === 'development'
         ? `mongodb://localhost:${mongoPort}`
-        : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.0ihfl.mongodb.net/test?retryWrites=true&w=majority`;
+        //: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.0ihfl.mongodb.net/test?retryWrites=true&w=majority`;
+        : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@ec2-3-127-135-219.eu-central-1.compute.amazonaws.com:27017,ec2-52-59-218-12.eu-central-1.compute.amazonaws.com:27017,ec2-3-69-45-11.eu-central1.compute.amazonaws.com:27017/?replicaSet=s0`
